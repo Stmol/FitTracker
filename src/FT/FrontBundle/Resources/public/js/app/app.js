2feb029c0db41473bd836c896b3b15a08219edce
Yury Stmol <dev@stmol.me>
2014-04-14 16:44:44 +0400

    Added main App JS file

---
+++ src/FT/FrontBundle/Resources/public/js/app/app.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Yury Smidovich (Stmol)
+ * Date: 13.03.14
+ * Project: Fittracker
+ * Url: http://stmol.me
+ * Email: dev@stmol.me
+ */
+
+window.FTApp = {
+  Models: {},
+  Collections: {},
+  Views: {},
+
+  init: function () {
+
+  }
+};
